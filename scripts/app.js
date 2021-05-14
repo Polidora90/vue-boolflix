693abcd4f57c5e7a777c90c6c91cd871415545ea
Polidora90 <alessandra.torry@gmail.com>
2021-05-14 09:38:43 +0200

    ottiene serie tv con la stessa ricerca

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ new Vue ({
     data: {
         query: "",
         movieList: [],
+        tvSeriesList: [],
         myFlagClass: "",
         defaultFlag: "flag-icon-default"
     },
@@ -10,17 +11,28 @@ new Vue ({
     },
     methods: {
 
-        //funzione che carica i film in base alla ricerca nell'input
-        doSearch() {
-            axios.get("https://api.themoviedb.org/3/search/movie", {
+        //funzione che crea due array separati da una sola chiamata grazie all'argomento searchType
+        makeAxiosSearch(searchType) {
+            axios.get("https://api.themoviedb.org/3/search/" + searchType, {
                 params: {
                     api_key: "c8a6a5493ec7223e34efa320cd602f6c",
                     query: this.query,
                     language: "it-IT"
                 }
             }).then((resp) => {
-                this.movieList = resp.data.results;
+                if (searchType === "movie") {
+                    this.movieList = resp.data.results;
+                } else if (searchType === "tv") {
+                    this.tvSeriesList = resp.data.results;
+                }
             })
+        },
+
+        //funzione che carica i film in base alla ricerca nell'input
+        //la funzione evoca a sua volta una funzione che stabilisce il parametro di ricerca tramite l'assegnazione dell'argomento
+        doSearch() {
+           this.makeAxiosSearch("movie");
+           this.makeAxiosSearch("tv");
         },
         
         //funzione che traduce la lingua dell'oggetto in classe per la flag
